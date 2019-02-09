a89ff6170208e14c6aab83efbedd9b357d97ac7d
Erwin Kok <erwinkok14@gmail.com>
2019-02-09 21:44:06 +0100

    First commit

---
+++ src/Minecraft/MainGameLoop.java
@@ -0,0 +1,24 @@
+package Minecraft;
+
+import RenderEngine.DisplayManager;
+import RenderEngine.MasterRenderer;
+import org.lwjgl.opengl.Display;
+
+public class MainGameLoop {
+
+    public static void main(String[] args) {
+        DisplayManager.createDisplay();
+
+        MasterRenderer renderer = new MasterRenderer();
+
+        while (!Display.isCloseRequested()) {
+
+            renderer.prepare();
+
+            DisplayManager.updateDisplay();
+
+        }
+
+        DisplayManager.closeDisplay();
+    }
+}
